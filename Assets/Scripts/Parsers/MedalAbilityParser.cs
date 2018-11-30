ff70648740ac83a941e8377adf03d6580b99128c
WaterKH <waterkh@outlook.com>
2018-11-30 16:33:39 -0500

    MedalAbilityParser
    + Committing to work at at home, not close to being done

--- Assets/Scripts/Parsers/MedalAbilityParser.cs
+++ Assets/Scripts/Parsers/MedalAbilityParser.cs
@@ -1,18 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
-public class MedalAbilityParser : MonoBehaviour {
+public class MedalAbilityParser : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Regex dealMultipleRegex = new Regex("Deals (\\d+) [\\w+\\s*]? hits");
+    public Regex dealOneRegex = new Regex("Deals an? [\\w+\\s*] hit");
+    public Regex raiseLowerRegex = new Regex(@"^(?:Lowers|Raises)(?: target(?:s')?(?:'s)?)? (\w+\W?\w+ (?:by \d+ tier\w? )?&*\s?)*for \d+ (?:turn(?:s)?|attack(?:s))");
+    public Regex lowerRegex = new Regex(@"Lowers targets' (\w+) by (\d+) tiers for (\d+) (\w+)");
+    public Regex updatedRaiseLowerRegex = new Regex(@"(\d+) (\w+): ([.+,*]+)");
+    public Regex innerUpdatedRaiseLowerRegex = new Regex("(\\w)[ target\\w+]? (\\w+) by (\\d+)");
+
+    public Regex inflictFixedRegex = new Regex("Inflicts[ a]? fixed");
+    // 358
+    public Regex inflictMoreRegex = new Regex("Inflicts more damage ([\\w+\\s*]+)");
+    
+    // 320
+    public Regex inflictTheHigherLowerBiggerSmallerRegex = new Regex("the ([\\w+]?) ([the slot number]?[your HP]?[your party]?)");
+    public Regex inflictTheMoreLess = new Regex("the ([more]?[less]?) ([special]?[SP]?[skills]?[Lux]?[turns]?[gauges used]?[gauges are full]?[enemies]?)");
+    public Regex inflictTheMoreGauges = new Regex("the more gauges ([\\w+\\s*])");
+    public Regex inflictToRegex = new Regex("to (\\w+) targets");
+
+    // 11
+    public Regex inflictWithRegex = new Regex("with ([\\d+]) enemy left");
+
+    // 27
+    public Regex inflictedInRegex = new Regex("in slot (\\d+)");
+    public Regex inflictedIfRegex = new Regex("if the target is ([\\w+])");
+
+    public Regex recoverRegex = new Regex("([\\w+]) recovers HP");
+    public Regex hpRecoverRegex = new Regex("HP recovery LV (\\d+)");
+    public Regex hpMaxRegex = new Regex("HP MAX");
+
+    public Regex fillRegex = new Regex("Fills (\\d+) gauges");
+    public Regex gaugeRegex = new Regex("Gauge \\+(\\d+)");
+
+    public Regex curesRegex = new Regex("Cures [own]? status ailments", RegexOptions.IgnoreCase);
+    
 
     public MedalAbility Parser(string abilityDescription)
     {
